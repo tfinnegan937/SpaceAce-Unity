10148f46ca96760bdcd153313024cacb2e55f7c8
Timothy Finnegan <tfinnegan@nevada.unr.edu>
2021-05-06 21:15:06 -0700

    Main Menu Implemented

---
+++ Assets/UIButtonScripts/SplashScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject instructions;
+    public GameObject mMenu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ViewInstructions()
+    {
+        instructions.SetActive(true);
+        mMenu.SetActive(false);
+    }
+
+    public void ReturnToMenu()
+    {
+        mMenu.SetActive(true);
+        instructions.SetActive(false);
+    }
+}
