6c13ae8ab3528e145863d512747d8217137d513d
Timothy Finnegan <tfinnegan@nevada.unr.edu>
2021-05-09 11:15:14 -0700

    Began implementing streamer AI

---
+++ Assets/Aspects/StreamerAI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreamerAI : AIAspect
+{
+    // Start is called before the first frame update
+    public Vector3 currentPoint;
+    public StreamerMoveMgr ptMgr;
+
+    [System.Serializable]
+    public enum State
+    {
+        Entering,
+        Selecting,
+        Moving,
+        Firing,
+        Leaving
+    }
+
+    public State state;
+    void Start()
+    {
+        state = State.Entering;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Entering:
+                Entering();
+                break;
+            case State.Selecting:
+                Selecting();
+                break;
+            case State.Moving:
+                Moving();
+                break;
+            case State.Firing:
+                Firing();
+                break;
+            case State.Leaving:
+                Leaving();
+                break;
+        }
+    }
+
+    void Entering()
+    {
+        
+    }
+
+    void Selecting()
+    {
+        
+    }
+
+    void Moving()
+    {
+        
+    }
+
+    void Firing()
+    {
+    }
+
+    void Leaving()
+    {
+        
+    }
+}
---
+++ Assets/Managers/StreamerMoveMgr.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreamerMoveMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform pointOne;
+    public Transform pointTwo;
+
+    public List<Vector3> positions;
+    void Start()
+    {
+        positions.Add(pointOne.position);
+        positions.Add(pointTwo.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetNearestPosition(Vector3 pos)
+    {
+        float distanceToOne = Vector3.Distance(pos, positions[0]);
+        float distanceToTwo = Vector3.Distance(pos, positions[1]);
+
+        if (distanceToTwo > distanceToOne)
+        {
+            return positions[0];
+        }
+
+        return positions[1];
+    }
+
+    public Vector3 GetNextPosition(Vector3 pos)
+    {
+        if (pos == positions[0])
+        {
+            return positions[1];
+        }
+
+        return positions[0];
+    }
+}
