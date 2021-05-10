71d06552fc59cad18f8c7c7e439bdc3901dc5001
Timothy Finnegan <tfinnegan@nevada.unr.edu>
2021-05-09 18:37:46 -0700

    Began implementing final boss AI

---
+++ Assets/Aspects/Final Boss.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [System.Serializable]
+    public enum BossState
+    {
+        Entering,
+        MovingToMainGunPt,
+        FiringMainGun,
+        MovingToMissilePt,
+        FiringMissiles,
+        SpawningStreamers,
+        ChargingDash,
+        Dashing,
+        Resetting,
+        Idling,
+        SelectingNextPattern,
+        TeleportingBack,
+    }
+
+    public BossState state;
+    public BossMissilePtMgr missilePtMgr;
+    public BossPtMgr ptMgr;
+    public Projectile missileProj;
+    public Projectile mainProj;
+    void Start()
+    {
+        state = BossState.Entering;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Handle State Machine
+        switch (state)
+        {
+            case BossState.Entering:
+                Entering();
+                break;
+            case BossState.MovingToMainGunPt:
+                MovingToMainGunPt();
+                break;
+            case BossState.MovingToMissilePt:
+                MovingToMissilePt();
+                break;
+            case BossState.FiringMissiles:
+                FiringMissiles();
+                break;
+            case BossState.FiringMainGun:
+                FiringMainGun();
+                break;
+            case BossState.SpawningStreamers:
+                SpawningStreamers();
+                break;
+            case BossState.ChargingDash:
+                ChargingDash();
+                break;
+            case BossState.Dashing:
+                Dashing();
+                break;
+            case BossState.Resetting:
+                Resetting();
+                break;
+            case BossState.Idling:
+                Idling();
+                break;
+            case BossState.SelectingNextPattern:
+                SelectingNextPattern();
+                break;
+            case BossState.TeleportingBack:
+                TeleportingBack();
+                break;
+        }
+    }
+
+    void Entering()
+    {
+        
+    }
+
+    void MovingToMainGunPt()
+    {
+        
+    }
+
+    void FiringMainGun()
+    {
+        
+    }
+
+    void MovingToMissilePt()
+    {
+        
+    }
+
+    void FiringMissiles()
+    {
+        
+    }
+
+    void SpawningStreamers()
+    {
+        
+    }
+
+    void ChargingDash()
+    {
+        
+    }
+
+    void Dashing()
+    {
+        
+    }
+
+    void Resetting()
+    {
+        
+    }
+
+    void Idling()
+    {
+        
+    }
+
+    void SelectingNextPattern()
+    {
+        
+    }
+
+    void TeleportingBack()
+    {
+        
+    }
+}
---
+++ Assets/Managers/BossMissilePtMgr.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMissilePtMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform pt1;
+    public Transform pt2;
+    public Transform pt3;
+    public Transform pt4;
+    private List<Vector3> pts;
+    private int fired = 0;
+
+    public static BossMissilePtMgr inst;
+
+    void Awake()
+    {
+        inst = this;
+    }
+    void Start()
+    {
+        pts.Add(pt1.position);
+        pts.Add(pt2.position);
+        pts.Add(pt3.position);
+        pts.Add(pt4.position);
+        Reset();
+
+
+    }
+    
+
+    public void Reset()
+    {
+        fired = 0;
+    }
+
+    public bool DoneFiring()
+    {
+        return (fired > pts.Count - 1);
+    }
+
+    public Vector3 GetNextPt()
+    {
+        Vector3 pt = pts[fired];
+        fired++;
+        return pt;
+    }
+}
